d8d1d71bbdf37decb8cfb3d6afaa13f99fd65067
adam <atomicpenguines@gmail.com>
2019-01-13 23:27:59 +0000

    basic create post view

--- blog/post_routes.py
+++ blog/post_routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, request, render_template, redirect, url_for
 from .post import Post
 
 post_blueprint = Blueprint('posts', __name__)
@@ -10,3 +10,23 @@ def get_posts():
     p = Post()
     posts = p.get_posts()
     return render_template('posts.html', posts=posts), 200
+
+
+@post_blueprint.route('/new', methods=['GET', 'POST'])
+def create_post():
+
+    if request.method == 'GET':
+        return render_template('new_post.html'), 200
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        print('title ', title, '\n', 'content ', content)
+        p = Post(title=title, content=content)
+        print(p)
+        p.create_post()
+
+        if p.get_post(p.post_id):
+            # redirect doesn't work here for some reason
+            return get_posts()
+        else:
+            return 'Oh no something went wrong :('
