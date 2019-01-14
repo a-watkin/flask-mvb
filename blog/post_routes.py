4c12d00bdf59146e9f6e4c9e74d3e538cc5a33b3
adam <atomicpenguines@gmail.com>
2019-01-14 02:34:28 +0000

    allowed html injection in posts template

--- blog/post_routes.py
+++ blog/post_routes.py
@@ -29,3 +29,22 @@ def create_post():
             return redirect(url_for('posts.get_posts'))
         else:
             return 'Oh no something went wrong :(', 404
+
+
+@post_blueprint.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit_post():
+    if request.method == 'GET':
+        return render_template('new_post.html'), 200
+    if request.method == 'POST':
+        # get the values
+        title = request.form['title']
+        content = request.form['content']
+        # make the psot
+        p = Post(title=title, content=content)
+        p.create_post()
+
+        if p.get_post(p.post_id):
+            # if you have a status code here it doesn't redirect
+            return redirect(url_for('posts.get_posts'))
+        else:
+            return 'Oh no something went wrong :(', 404
