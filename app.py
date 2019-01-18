163d2d6d44dbceae3f682daa00604cefa1ce92d1
a-watkin <awatkin.work@gmail.com>
2019-01-18 09:05:56 +0000

    some cleanup and testing server

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 import subprocess
 from flask import Flask
-# this only sort of works
-# from flaskext.markdown import Markdown
 
 DEVELOPMENT = False
 
@@ -10,10 +8,6 @@ from blog.post_routes import post_blueprint
 from blog.user_routes import user_blueprint
 
 app = Flask(__name__)
-# Markdown(app)
-
-# template_folder='blog/templates',
-# static_folder = 'blog/static'
 
 app.register_blueprint(post_blueprint, url_prefix="/posts")
 app.register_blueprint(user_blueprint, url_prefix="/user")
@@ -33,8 +27,8 @@ def test():
 if __name__ == '__main__':
     export_settings = [
         "export FLASK_APP=app.py",
-        "export FLASK_ENV=development",
-        "export FLASK_DEBUG=1"
+        # "export FLASK_ENV=development",
+        # "export FLASK_DEBUG=1"
     ]
 
     for command in export_settings:
